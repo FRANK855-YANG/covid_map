cb304ba85400ed94a8e821fd6e92a759a1e7db46
frank855 <zhenyue.yang@outlook.com>
2022-06-22 00:06:43 +0800

    projext setup

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import CovidMap from './components/CovidMap.vue'
+</script>
+
+<template>
+  <CovidMap></CovidMap>
+</template>
+
+<style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+}
+</style>
